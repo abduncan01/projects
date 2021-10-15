32712bfaa32815c74c7b7cd6584a083d342d4d1e
abduncan01 <90365234+abduncan01@users.noreply.github.com>
2021-10-14 22:36:20 -0400

    Update assignment2.js

--- assignment02/assignment2.js
+++ assignment02/assignment2.js
@@ -7,22 +7,26 @@ const list = document.createElement('list');
 
 let title = document.querySelector('#title').value;
 let priority = document.querySelector('#priority').value;
-let status;// = document.querySelectorAll('name[status]').value;
- let inputs = document.querySelectorAll('[name=task-status]');
+let status = document.querySelectorAll('name[status]').value;
+ let inputs = document.querySelectorAll('[name=status]');
 for (let i = 0; i< inputs.length;i++){
 if (inputs[i].checked){
 status = inputs[i].value;
 }
-let new_task_html = `<div class = "newTask"> <span> ${title} </span> <span>| ${priority} </span> <span> | ${status} </span> </div>
+let new_task_html = `<div class = "newTask"> 
+<span> ${title} </span>
+<span>| ${priority} </span> 
+<span> | ${status} </span> 
+</div>
   
   
-  <button class = "Completed"> Mark as Complete </button> <button class = "remove"> Remove </button>`;
-let priority2 = ` <span> ${priority} </span>`;
+  <button class = "Completed"> Task Completed </button> <button class = "remove"> Remove Task </button>`;
+let priority2 = `<span> ${priority} </span>`;
 let status2 = `<span> ${status} </span>`;
 list.innerHTML = new_task_html
 list.innerHTML = document.querySelector('#task').value;
 task1.push(title);
-document.querySelector('#task_list').append(list);
+document.querySelector('#taskList').append(list);
 document.querySelector('#task-title').value = '';
 return false;
 }
